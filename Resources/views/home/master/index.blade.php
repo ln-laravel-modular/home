9bf5372fd106bb0949e136972488080e63e53e33
langnang <langnang.chen@outlook.com>
2024-03-19 17:09:53 +0800

    home

--- Resources/views/home/master/index.blade.php
+++ Resources/views/home/master/index.blade.php
@@ -1,21 +1,8 @@
 @extends($config['slug'] . '::layouts.' . $config['layout'])
 
-@section('content')
+@section('main')
   <div
     class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    @if (Route::has('login'))
-      <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-        @auth
-          <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-        @else
-          <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-          @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-          @endif
-        @endauth
-      </div>
-    @endif
 
     <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
       <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
@@ -27,102 +14,7 @@
           </g>
         </svg>
       </div>
-
-      <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-        <div class="grid grid-cols-1 md:grid-cols-2">
-          <div class="p-6">
-            <div class="flex items-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                <path
-                  d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253">
-                </path>
-              </svg>
-              <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs"
-                  class="underline text-gray-900 dark:text-white">Documentation</a></div>
-            </div>
-
-            <div class="ml-12">
-              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are
-                new to the framework or have previous experience with Laravel, we recommend reading all of the
-                documentation from beginning to end. <a href="https://laravelmodules.com/docs/v8"
-                  class="underline">laravel-modules</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-            <div class="flex items-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                <path
-                  d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z">
-                </path>
-                <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
-              </svg>
-              <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com"
-                  class="underline text-gray-900 dark:text-white">Laracasts</a></div>
-            </div>
-
-            <div class="ml-12">
-              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them
-                out, see for yourself, and massively level up your development skills in the process.
-              </div>
-            </div>
-          </div>
-
-          <div class="p-6 border-t border-gray-200 dark:border-gray-700">
-            <div class="flex items-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                <path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z">
-                </path>
-              </svg>
-              <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/"
-                  class="underline text-gray-900 dark:text-white">Laravel News</a></div>
-            </div>
-
-            <div class="ml-12">
-              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                Laravel News is a community driven portal and newsletter aggregating all of the latest and most
-                important news in the Laravel ecosystem, including new package releases and tutorials.
-              </div>
-            </div>
-          </div>
-
-          <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-            <div class="flex items-center">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                <path
-                  d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
-                </path>
-              </svg>
-              <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</div>
-            </div>
-
-            <div class="ml-12">
-              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                Laravel's robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com"
-                  class="underline">Forge</a>, <a href="https://vapor.laravel.com" class="underline">Vapor</a>, <a
-                  href="https://nova.laravel.com" class="underline">Nova</a>, and <a href="https://envoyer.io"
-                  class="underline">Envoyer</a> help you take your projects to the next
-                level. Pair them with powerful
-                open source libraries like <a href="https://laravel.com/docs/billing" class="underline">Cashier</a>, <a
-                  href="https://laravel.com/docs/dusk" class="underline">Dusk</a>, <a
-                  href="https://laravel.com/docs/broadcasting" class="underline">Echo</a>, <a
-                  href="https://laravel.com/docs/horizon" class="underline">Horizon</a>, <a
-                  href="https://laravel.com/docs/sanctum" class="underline">Sanctum</a>, <a
-                  href="https://laravel.com/docs/telescope" class="underline">Telescope</a>, and more.
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-      <div class="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ml:-1 mr:-1">
+      <div class="mt-8 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 ml:-1 mr:-1">
         @foreach (Module::all() ?? [] as $moduleName => $module)
           @if (Module::isEnabled($moduleName))
             <a class="m-1 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg"
@@ -131,7 +23,7 @@
                 <div class="flex items-center">
                 @empty(Config::get(strtolower($moduleName) . '.ico'))
                   <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                    stroke-width="2" viewBox="0 0 1024 1024" class="w-8 h-8 text-gray-500">
+                    stroke-width="2" viewBox="0 0 1024 1024" class="w-8 h-8 text-gray-500 d-none">
                     <path
                       d="M871.9 259.8L552 75.9c-24-16-56-16-80 0L152.1 259.8c-24 16-40 40-40 68v367.9c0 28 16 56 40 68L472 947.6c24 16 56 16 80 0l319.9-183.9c24-16 40-40 40-68V327.8c0-28-16-56-40-68z m-1.1 418c0 25.1-14.4 46.7-35.9 61L547.8 904c-21.5 14.4-50.3 14.4-71.8 0L188.8 738.9c-21.5-10.8-35.9-35.9-35.9-61V347.6c0-25.1 14.4-46.7 35.9-61L476 121.4c21.5-14.4 50.3-14.4 71.8 0L835 286.5c21.5 10.8 35.9 35.9 35.9 61v330.3z"
                       p-id="4277"></path>
